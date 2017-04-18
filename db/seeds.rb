9cb4f598c69717da8d5954d06a62450fc4ad6263
Paula <pauladenouden@gmail.com>
2017-04-18 20:35:29 +0200

    Add match model and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 User.delete_all
+Match.delete_all
 
-AdminMandy = User.create(first_name: "Mandy", last_name: "Admin", student: false, admin: true, email: "mandy@express.biz", password: "abc123")
-StudentSteve = User.create(first_name: "Steve", last_name: "Student", student: true, admin: false, email: "steve@express.biz", password: "abc123")
-Jack = User.create(first_name: "Jack", last_name: "Rabbit", student: true, admin: false, email: "jack@express.biz", password: "abc123")
-Jane = User.create(first_name: "Jane", last_name: "GI", student: true, admin: false, email: "jane@express.biz", password: "abc123")
-Amanda = User.create(first_name: "Amanda", last_name: "Adnama", student: true, admin: false, email: "amanda@express.biz", password: "abc123")
-Dave = User.create(first_name: "Dave", last_name: "Evad", student: true, admin: false, email: "steve@express.biz", password: "abc123")
+AdminMandy = User.create(first_name: "Mandy", last_name: "Admin", admin: true, email: "mandy@express.biz", password: "abc123")
+StudentSteve = User.create(first_name: "Steve", last_name: "Student", admin: false, email: "steve@express.biz", password: "abc123")
+Jack = User.create(first_name: "Jack", last_name: "Rabbit", admin: false, email: "jack@express.biz", password: "abc123")
+Jane = User.create(first_name: "Jane", last_name: "GI",  admin: false, email: "jane@express.biz", password: "abc123")
+Amanda = User.create(first_name: "Amanda", last_name: "Adnama",  admin: false, email: "amanda@express.biz", password: "abc123")
+Dave = User.create(first_name: "Dave", last_name: "Evad", admin: false, email: "steve@express.biz", password: "abc123")
 
 puts "Created users"
+
+match1 = Match.create(date: Date.new, users: [StudentSteve, Jack])
+match2 = Match.create(date: Date.new, users: [Dave, Amanda])
+
+puts "Created matches"
