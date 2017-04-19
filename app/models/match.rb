5ddb69a713ab0a198bb48e73f33a3645a56011c3
Paula <pauladenouden@gmail.com>
2017-04-19 20:34:30 +0200

    bug fix

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,8 +4,8 @@ class Match < ApplicationRecord
   def self.matchable_students(matchables, current_student)
     least_matched = self.minimum_matched(matchables, current_student)
     matchables = matchables.select {|matchable_student| matchable_student != current_student && self.times_matched(current_student, matchable_student) <= least_matched }
-    debugger
-    matchables
+    puts "matchable students:"
+    matchables.each {|student| puts student.name }
   end
 
   def self.minimum_matched(students, current_student)
@@ -21,31 +21,33 @@ class Match < ApplicationRecord
     Match.select{|match| match.users.include?(matchable_student) && match.users.include?(current_student)}.length
   end
 
-  def self.make_match(students)
-    debugger
-    students.sample
+  def self.make_match(matchable_students)
+    matchable_students.sample
   end
 
   def self.match_student(students, current_student, date)
-    if current_student.matches.select{|match| match[:date] == date }.length != 0
-      matchables = students.select{|student| student != current_student }
-      debugger
+    if current_student.matches.select{|match| match[:date].strftime("%F") == date }.length > 0
+      students = students.select{|student| student != current_student }
+      puts "student #{current_student.name} already matched on this day"
+      puts '-'* 50
     else
       matchables = matchable_students(students, current_student)
       match = self.make_match(matchables)
       puts "Student #{current_student.name} matched with #{match.name}"
       Match.create(date: date, users: [match, current_student])
-      matchables = matchables.select{|student| student != match }
+      students = students.select{|student| student != match && student != current_student}
     end
     puts "Unmatched students:"
-    matchables.each {|student| puts student.name }
-
-    return if matchables.length <= 1
-    match_student(matchables, matchables.first, date)
+    students.each {|student| puts student.name }
+    puts "number of students unmatched: #{students.length}"
+    return if students.length <= 1
+    self.match_student(students, students.first, date)
   end
 
   def self.generate(date)
+    puts "About to match students"
     students = User.select{|hash| hash[:admin] == false }
+    puts "students to be matched: #{students.length}"
     self.match_student(students, students.first, date)
     puts "yay made the matches"
   end
