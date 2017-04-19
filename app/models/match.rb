60f17e04528ad2098e2d4c47ac78383a1f33ad18
Paula <pauladenouden@gmail.com>
2017-04-19 20:44:41 +0200

    commit after rebase

--- app/models/match.rb
+++ app/models/match.rb
@@ -26,8 +26,6 @@ class Match < ApplicationRecord
   def self.match_student(students, current_student, date)
     if current_student.matches.select{|match| match[:date].strftime("%F") == date }.length > 0
       students = students.select{|student| student != current_student }
-      puts "student #{current_student.name} already matched on this day"
-      puts '-'* 50
     else
       matchables = matchable_students(students, current_student)
       match = self.make_match(matchables)
@@ -35,17 +33,12 @@ class Match < ApplicationRecord
       Match.create(date: date, users: [match, current_student])
       students = students.select{|student| student != match && student != current_student}
     end
-    puts "Unmatched students:"
-    students.each {|student| puts student.name }
-    puts "number of students unmatched: #{students.length}"
     return if students.length <= 1
     self.match_student(students, students.first, date)
   end
 
   def self.generate(date)
-    puts "About to match students"
     students = User.select{|hash| hash[:admin] == false }
-    puts "students to be matched: #{students.length}"
     self.match_student(students, students.first, date)
     puts "yay made the matches"
   end
