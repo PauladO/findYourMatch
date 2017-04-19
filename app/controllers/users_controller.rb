4c736da7f8b95762aafc84cdcc47ebce5eb4f253
Paula <pauladenouden@gmail.com>
2017-04-19 21:07:55 +0200

    Add button to promote/demote. not yet functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User admin rights changed"
+    else
+      redirect_to users_path, notice: "Could not update user"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:admin, :id)
+    end
 end
