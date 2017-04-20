d90a1f8c92056222b906f667d8ea5bf0134835c0
Paula <pauladenouden@gmail.com>
2017-04-20 10:54:23 +0200

    Add toggle admin status button, install factory girl, faker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,20 @@
 class UsersController < ApplicationController
   # before_action :authenticate_user!
-
+  before_action :set_user, only: [:show, :update]
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to users_path, notice: "User admin rights changed"
-    else
-      redirect_to users_path, notice: "Could not update user"
-    end
+    User.toggle_admin(params[:id])
+    redirect_to users_path
   end
 
   private
-    def user_params
-      params.require(:user).permit(:admin, :id)
+    def set_user
+      @user = User.find(params[:id])
     end
 end
